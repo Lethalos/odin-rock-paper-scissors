b9da394f66db1fe8ba80ecd4ae0db234c648128e
KaanOzdogan <necdetkaanoz@hotmail.com>
2022-09-27 21:57:24 +0300

    Play one round at console

---
+++ script.js
@@ -0,0 +1,43 @@
+let computerSelection;
+let playerSelection;
+
+let getComputerChoice = () => {
+  let choice = Math.floor(Math.random() * 3);
+
+  if (choice == 0) {
+    return "rock";
+  } else if (choice == 1) {
+    return "paper";
+  } else if (choice == 2) {
+    return "scissors";
+  }
+};
+
+computerSelection = getComputerChoice();
+
+playerSelection = prompt("Enter rock or paper or scissors");
+
+let playRound = (playerSelection, computerSelection) => {
+  let selection = playerSelection.toLowerCase();
+
+  if (playerSelection == computerSelection) {
+    console.log("Draw!");
+    return;
+  }
+
+  if (selection == "rock" && computerSelection == "scissors") {
+    console.log(`You Win ${selection} beats ${computerSelection}`);
+  } else if (selection == "rock" && computerSelection == "paper") {
+    console.log(`You Lose ${computerSelection} beats ${selection}`);
+  } else if (selection == "paper" && computerSelection == "rock") {
+    console.log(`You Win ${selection} beats ${computerSelection}`);
+  } else if (selection == "paper" && computerSelection == "scissors") {
+    console.log(`You Lose ${computerSelection} beats ${selection}`);
+  } else if (selection == "scissors" && computerSelection == "paper") {
+    console.log(`You Win ${selection} beats ${computerSelection}`);
+  } else if (selection == "scissors" && computerSelection == "rock") {
+    console.log(`You Win ${computerSelection} beats ${selection}`);
+  }
+};
+
+playRound(playerSelection, computerSelection);
